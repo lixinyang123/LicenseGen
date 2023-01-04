137cee630e9eaaba9e5d5d3318635fa2172eab82
LLLXY <lixinyangemil@outlook.com>
2023-01-04 19:22:06 +0800

    Update SecurityService.cs

--- KeyGen/Services/SecurityService.cs
+++ KeyGen/Services/SecurityService.cs
@@ -72,7 +72,7 @@ namespace KeyGen.Services
         }
 
         // 补齐数据用于解码
-        public string ComplementBase64(string text)
+        private string ComplementBase64(string text)
         {
             int count = text.Length % 4;
             return count == 0 ? text : text + new string('=', count);
